2d910dd4aba1934ea1242cd261d3faa6081bb93c
Freddy <frederick.linn@colorado.edu>
2022-06-04 20:52:34 -0600

    adding 07 solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(inputTemp) {
+  celsiusTemp = (inputTemp - 32) * (5/9);
+  return Math.round(celsiusTemp * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(inputTemp) {
+  fTemp = inputTemp * (9/5) + 32;
+  return Math.round(fTemp * 10) / 10;
 };
 
 // Do not edit below this line
