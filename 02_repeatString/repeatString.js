6539ea96ed92a4593d76f08aedb95032c9ed3b66
Freddy <frederick.linn@colorado.edu>
2022-06-04 19:26:58 -0600

    adding 02 solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,17 @@
-const repeatString = function() {
+const repeatString = function(inputString,num) {
+
+    let outputString = "";
+
+    if(num >= 0){
+        for(let i = 0; i < num; i++){
+            outputString += inputString;
+        }
+    }
+    else{
+        return "ERROR";
+    }
+
+    return outputString;
 
 };
 
