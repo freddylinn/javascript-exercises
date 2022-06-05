9bbff002f596b904734d3a47d5f8e43d724a5479
Freddy <frederick.linn@colorado.edu>
2022-06-04 20:39:45 -0600

    adding 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,19 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+
+    if(typeof(firstNum) != "number" || typeof(secondNum) != "number" || firstNum < 0 || secondNum < 0){
+        return "ERROR";
+    }
+
+    const start = Math.min(firstNum,secondNum);
+    const end = Math.max(firstNum,secondNum);
+
+    let sum = 0;
+
+    for(let i = start; i <= end; i++){
+        sum += i;
+    }
+
+    return sum;
 
 };
 
