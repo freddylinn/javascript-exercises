82b27b8d8b6b5dbbc8d52aa68ef8763d86702a3c
Freddy <frederick.linn@colorado.edu>
2022-06-13 21:42:12 -0600

    adding 09 solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+    const filtered = phrase.toLowerCase().replace(/[^a-z]/g, "");
+    const comparison = filtered.split("");
+    comparison.reverse();
+    final = comparison.join("");
+    return final == filtered;
 };
 
 // Do not edit below this line
